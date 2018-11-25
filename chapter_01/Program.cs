1aa61b224ac11ecd999f838e0e9ab6fba079495d
Ycf0511 <45328999+Ycf0511@users.noreply.github.com>
2018-11-25 17:46:46 +0800

    1
    1

---
+++ chapter_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Point
+ {
+Point[] s=new Point[1000];
+public int x,y;
+
+ }
+namespace p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point [] a=new Point[1000];
+            for (int i=0;i<a.Length;i++)
+            {
+Console.WriteLine(a[i]);
+            } 
+      
+{
+
+
+}
+        }
+    }
+}
