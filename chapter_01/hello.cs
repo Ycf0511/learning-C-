e54b9aee7fa71418ea18b34623367455aa3c4b77
Ycf0511 <45328999+Ycf0511@users.noreply.github.com>
2018-11-25 18:06:10 +0800

    first program
    first c# program

---
+++ chapter_01/hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hellowlord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
